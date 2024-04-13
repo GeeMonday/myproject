12ac8f166bea7b6358d3f3d9c3551c763c9559e0
George Monday <georgebenmonday@gmail.com>
2024-04-13 15:16:07 -0500

    chaned admin view

--- Admin Dashboard/players/manage_players.php
+++ Admin Dashboard/players/manage_players.php
@@ -79,7 +79,7 @@ $players = $statement->fetchAll(PDO::FETCH_ASSOC);
     <?php include('nav_admin.php'); ?>
     <div class="container mt-5">
             <div id="header">
-                <h2>Page Administration</h2>
+                <h2>Manage  Players</h2>
             </div>
             <!-- Button to create a new player -->
             <div class="mt-4 mb-4">
